356d1eab44c7111d51785f95b1a431e39efe5764
manikanta <manikanta@mani>
2025-10-23 13:44:45 +0530

    ok

---
+++ Helloservlet/src/main/java/HelloServlet.java
@@ -0,0 +1,23 @@
+import java.io.IOException;
+import java.io.PrintWriter;
+
+import jakarta.servlet.ServletException;
+import jakarta.servlet.http.HttpServlet;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+
+	public class HelloServlet extends HttpServlet {
+
+	    public void doGet(HttpServletRequest request, HttpServletResponse response)
+	            throws ServletException, IOException {
+
+	        response.setContentType("text/html");
+	        PrintWriter out = response.getWriter();
+
+	        out.println("<html><body>");
+	        out.println("<h1>Hello from Servlet!</h1>");
+	        out.println("<p>This is a simple Java Servlet example.</p>");
+	        out.println("</body></html>");
+	    }
+	}
+
